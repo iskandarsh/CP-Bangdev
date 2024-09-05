e145607eb52a00767c1d1aa5e882211cfeabf813
Arthur Darwanto <75304260+arthur-da-1205@users.noreply.github.com>
2024-09-05 11:07:01 +0700

    improve navbar and navigation

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,9 +1,15 @@
-/* eslint-disable jsx-a11y/anchor-is-valid */
 import React, { useState } from 'react';
 import logo from '@assets/images/logo/logo.png';
+import { useLocation, useNavigate } from 'react-router-dom';
 
 const Navbar: React.FC = () => {
   const [toggle, setToggle] = useState(false);
+  const location = useLocation();
+  const navigate = useNavigate();
+
+  const isActive = (path: string) => {
+    return location.pathname === path;
+  };
 
   return (
     <>
@@ -34,36 +40,43 @@ const Navbar: React.FC = () => {
           <div className={`${toggle ? 'block' : 'hidden'} w-full md:block md:w-auto`} id="navbar-default">
             <ul className="mt-4 flex flex-col rounded-lg border border-gray-100 bg-gray-50 p-4 font-medium dark:border-gray-700 dark:bg-gray-800 md:mt-0 md:flex-row md:space-x-8 md:border-0 md:bg-white md:p-0 md:dark:bg-gray-900 lg:gap-10 rtl:space-x-reverse">
               <li>
-                <a href="/" className="block px-2 py-2" aria-current="page">
+                <span
+                  className={`block cursor-pointer rounded px-2 py-2 ${isActive('/') ? 'text-red-600' : 'text-gray-900'} md:dark:hover:text-white-500 hover:bg-gray-100 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:border-0 md:hover:bg-transparent md:hover:text-red-400 md:dark:hover:bg-transparent`}
+                  aria-current="page"
+                  onClick={() => navigate('/')}
+                >
                   Home
-                </a>
+                </span>
               </li>
               <li>
-                <a
-                  href="/about"
-                  className="block rounded px-2 py-2 text-gray-900 hover:bg-gray-100 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:border-0 md:hover:bg-transparent md:hover:text-blue-700 md:dark:hover:bg-transparent md:dark:hover:text-blue-500"
+                <span
+                  className={`block cursor-pointer rounded px-2 py-2 ${isActive('/about') ? 'text-red-600' : 'text-gray-900'} md:dark:hover:text-white-500 hover:bg-gray-100 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:border-0 md:hover:bg-transparent md:hover:text-red-400 md:dark:hover:bg-transparent`}
+                  aria-current="page"
+                  onClick={() => navigate('/')}
                 >
                   About Us
-                </a>
+                </span>
               </li>
               <li>
-                <a
-                  href="#"
-                  className="block rounded px-2 py-2 text-gray-900 hover:bg-gray-100 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:border-0 md:hover:bg-transparent md:hover:text-blue-700 md:dark:hover:bg-transparent md:dark:hover:text-blue-500"
+                <span
+                  className={`block cursor-pointer rounded px-2 py-2 ${isActive('/') ? 'text-red-600' : 'text-gray-900'} md:dark:hover:text-white-500 hover:bg-gray-100 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:border-0 md:hover:bg-transparent md:hover:text-red-400 md:dark:hover:bg-transparent`}
+                  aria-current="page"
+                  onClick={() => navigate('/')}
                 >
                   Services
-                </a>
+                </span>
               </li>
               <li>
-                <a
-                  href="/career"
-                  className="block rounded px-2 py-2 text-gray-900 hover:bg-gray-100 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:border-0 md:hover:bg-transparent md:hover:text-blue-700 md:dark:hover:bg-transparent md:dark:hover:text-blue-500"
+                <span
+                  className={`block cursor-pointer rounded px-2 py-2 ${isActive('/career') ? 'text-red-600' : 'text-gray-900'} md:dark:hover:text-white-500 hover:bg-gray-100 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:border-0 md:hover:bg-transparent md:hover:text-red-400 md:dark:hover:bg-transparent`}
+                  aria-current="page"
+                  onClick={() => navigate('/career')}
                 >
                   Career
-                </a>
+                </span>
               </li>
               <li>
-                <a href="#" className="block rounded-[200px] bg-red-600 px-2 py-2 text-center text-white md:px-5">
+                <a href="/" className="block rounded-[200px] bg-red-600 px-2 py-2 text-center text-white md:px-5">
                   Contact Us
                 </a>
               </li>
